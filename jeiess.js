aaa03abb85e20f276311e6d7eab4c39771f1a454
Luis Gustavo <lgvs1307@gmail.com>
2024-01-23 03:13:31 -0300

    first commit

---
+++ jeiess.js
@@ -0,0 +1,25 @@
+function insert(num) {
+  var numero = document.getElementById("res").innerHTML;
+  document.getElementById("res").innerHTML = numero + num;
+}
+
+function clean() {
+  document.getElementById("res").innerHTML = "";
+}
+
+function back() {
+  var resultado = document.getElementById("res").innerHTML;
+  document.getElementById("res").innerHTML = resultado.substring(
+    0,
+    resultado.length - 1
+  );
+}
+
+function calcular() {
+  var resultado = document.getElementById("res").innerHTML;
+  if (resultado) {
+    document.getElementById("res").innerHTML = eval(resultado);
+  } else {
+    document.getElementById("res").innerHTML = "error";
+  }
+}
